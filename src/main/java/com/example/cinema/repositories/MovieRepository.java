bcd0b520268cf6cde31da41030a988ad2b6fcdd4
Tadiwanashe Dzvoti <tadiwadzvoti@hotmail.com>
2024-02-27 18:45:51 +0000

    Remove unused imports.

--- src/main/java/com/example/cinema/repositories/MovieRepository.java
+++ src/main/java/com/example/cinema/repositories/MovieRepository.java
@@ -2,8 +2,6 @@ package com.example.cinema.repositories;
 
 import com.example.cinema.models.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
